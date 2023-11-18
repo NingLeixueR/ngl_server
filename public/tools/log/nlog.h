28b364a617911b3b6a9b446f98b89b5daed08260
NingLeiXueR <348634371@qq.com>
2023-11-19 02:19:31 +0800

    定义linux上兼容的宏
    定义linux上兼容的宏

--- public/tools/log/nlog.h
+++ public/tools/log/nlog.h
@@ -50,7 +50,11 @@ namespace ngl
 #define DEF_LOG_MAX_LINE		(100000)
 #define DEF_LOG_PRINTF			(true)
 
-#define dlogmsg(ELOG_LEVEL, ISSYSYTEM, FORMAT,...)										\
+
+
+
+#if defined(WIN32)||defined(WINCE)||defined(WIN64)
+# define dlogmsg(ELOG_LEVEL, ISSYSYTEM, FORMAT,...)										\
 	{																					\
 		if(DEF_LOG_LEVEL <= ELOG_LEVEL)													\
 		{																				\
@@ -61,23 +65,48 @@ namespace ngl
 			ngl::nlog::getInstance().plog(ELOG_LEVEL, llogformat, ISSYSYTEM);			\
 		}																				\
 	}
-
-#define dlogmsg_bi(FORMAT,...)															\
+# define dlogmsg_bi(FORMAT,...)															\
 	{																					\
 		ngl::logformat llogformat;														\
 		llogformat.format("src", FORMAT,##__VA_ARGS__);									\
 		ngl::nlog::getInstance().plog_bi(ngl::ELOG_INFO, llogformat);					\
 	}
-
-#define LogDebug(FORMAT,...)			dlogmsg(ngl::ELOG_DEBUG, false, FORMAT,##__VA_ARGS__)
-#define LogError(FORMAT,...)			dlogmsg(ngl::ELOG_ERROR, false, FORMAT,##__VA_ARGS__)
-#define LogInfo(FORMAT,...)				dlogmsg(ngl::ELOG_INFO, false,FORMAT, ##__VA_ARGS__)
-#define LogWarn(FORMAT,...)				dlogmsg(ngl::ELOG_WARN, false, FORMAT,##__VA_ARGS__)
-
+# define LogDebug(FORMAT,...)			dlogmsg(ngl::ELOG_DEBUG, false, FORMAT,##__VA_ARGS__)
+# define LogError(FORMAT,...)			dlogmsg(ngl::ELOG_ERROR, false, FORMAT,##__VA_ARGS__)
+# define LogInfo(FORMAT,...)			dlogmsg(ngl::ELOG_INFO, false,FORMAT, ##__VA_ARGS__)
+# define LogWarn(FORMAT,...)			dlogmsg(ngl::ELOG_WARN, false, FORMAT,##__VA_ARGS__)
 #define LogLocalDebug(FORMAT,...)		dlogmsg(ngl::ELOG_DEBUG, true, FORMAT,##__VA_ARGS__)
 #define LogLocalError(FORMAT,...)		dlogmsg(ngl::ELOG_ERROR, true, FORMAT,##__VA_ARGS__)
-#define LogLocalInfo(FORMAT,...)		dlogmsg(ngl::ELOG_INFO, true,FORMAT, ##__VA_ARGS__)
+#define LogLocalInfo(FORMAT,...)		dlogmsg(ngl::ELOG_INFO, true,FORMAT,##__VA_ARGS__)
 #define LogLocalWarn(FORMAT,...)		dlogmsg(ngl::ELOG_WARN, true, FORMAT,##__VA_ARGS__)
+#else
+# define dlogmsg(ELOG_LEVEL, ISSYSYTEM, FORMAT,...)										\
+	{																					\
+		if(DEF_LOG_LEVEL <= ELOG_LEVEL)													\
+		{																				\
+			ngl::logformat llogformat;													\
+			LogSrcPos																	\
+			llogformat.data("head") = ngl::localtime::time2msstr("%Y-%m-%d %H:%M:%S");	\
+			llogformat.format("src", FORMAT __VA_OPT__(,) ##__VA_ARGS__);				\
+			ngl::nlog::getInstance().plog(ELOG_LEVEL, llogformat, ISSYSYTEM);			\
+		}																				\
+	}
+# define dlogmsg_bi(FORMAT,...)															\
+	{																					\
+		ngl::logformat llogformat;														\
+		llogformat.format("src", FORMAT __VA_OPT__(,) ##__VA_ARGS__);					\
+		ngl::nlog::getInstance().plog_bi(ngl::ELOG_INFO, llogformat);					\
+	}
+# define LogDebug(FORMAT,...)			dlogmsg(ngl::ELOG_DEBUG, false, FORMAT __VA_OPT__(,) ##__VA_ARGS__)
+# define LogError(FORMAT,...)			dlogmsg(ngl::ELOG_ERROR, false, FORMAT __VA_OPT__(,) ##__VA_ARGS__)
+# define LogInfo(FORMAT,...)			dlogmsg(ngl::ELOG_INFO, false,FORMAT __VA_OPT__(,)  ##__VA_ARGS__)
+# define LogWarn(FORMAT,...)			dlogmsg(ngl::ELOG_WARN, false, FORMAT __VA_OPT__(,) ##__VA_ARGS__)
+#define LogLocalDebug(FORMAT,...)		dlogmsg(ngl::ELOG_DEBUG, true, FORMAT __VA_OPT__(,) ##__VA_ARGS__)
+#define LogLocalError(FORMAT,...)		dlogmsg(ngl::ELOG_ERROR, true, FORMAT  __VA_OPT__(,) ##__VA_ARGS__)
+#define LogLocalInfo(FORMAT,...)		dlogmsg(ngl::ELOG_INFO, true,FORMAT __VA_OPT__(,) ##__VA_ARGS__)
+#define LogLocalWarn(FORMAT,...)		dlogmsg(ngl::ELOG_WARN, true, FORMAT  __VA_OPT__(,) ##__VA_ARGS__)
+#endif
+
 
 #define LogBI(FORMAT,...)				dlogmsg_bi(FORMAT,##__VA_ARGS__)
 
