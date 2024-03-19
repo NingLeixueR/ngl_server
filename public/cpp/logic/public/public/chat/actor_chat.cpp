3d52c522b22f7159841ebfb106c70e7293438453
NingLeiXueR <348634371@qq.com>
2024-03-19 10:57:32 +0800

    优化-规范化命名

--- public/cpp/logic/public/public/chat/actor_chat.cpp
+++ public/cpp/logic/public/public/chat/actor_chat.cpp
@@ -78,4 +78,85 @@ namespace ngl
 		m_update_chatitem.clear();
 		return true;
 	}
+
+	bool actor_chat::handle(message<mforward<pbnet::PROBUFF_NET_CHAT>>& adata)
+	{
+		pbnet::PROBUFF_NET_CHAT& recv = *adata.m_data->data();
+		LogLocalError("recv [pbnet::PROBUFF_NET_CHAT] type:%", recv.m_type())
+
+			if (recv.m_type() == pbnet::chat_speak)
+			{
+				auto pro = std::make_shared<pbnet::PROBUFF_NET_CHAT_RESPONSE>();
+				pro->set_m_type(pbnet::chat_speak);
+				pro->set_m_channelid(recv.m_channelid());
+				pro->set_m_stat(false);
+
+				tab_chat* ltab = allcsv::tab<tab_chat>(recv.m_channelid());
+				if (ltab == nullptr)
+				{
+					send_client(adata.m_data->identifier(), pro);
+					return true;
+				}
+
+				roleitem* lroleitem = type_roleitems::get_roleinfo(adata.m_data->identifier());
+				if (lroleitem == nullptr)
+				{
+					return true;
+				}
+
+				int ltemputc = localtime::gettime() - lroleitem->m_lastspeakutc;
+				if (ltemputc < ltab->m_time)
+				{
+					send_client(adata.m_data->identifier(), pro);
+					return true;
+				}
+				lroleitem->m_lastspeakutc = localtime::gettime();
+
+				std::list<pbnet::chatitem>& lvec = m_update_chatitem[recv.m_channelid()];
+				lvec.push_back(pbnet::chatitem());
+				pbnet::chatitem& lchatitem = *lvec.rbegin();
+
+				lchatitem.set_m_rolename(lroleitem->m_info.m_name());
+				lchatitem.set_m_utc(localtime::gettime());
+				lchatitem.set_m_content(recv.m_content());
+				lchatitem.set_m_roleid(lroleitem->m_info.m_id());
+
+				pro->set_m_stat(true);
+				send_client(adata.m_data->identifier(), pro);
+			}
+			else if (recv.m_type() == pbnet::get_chat_list)
+			{
+				auto pro = std::make_shared<pbnet::PROBUFF_NET_CHAT_RESPONSE>();
+				pro->set_m_stat(false);
+				pro->set_m_type(pbnet::get_chat_list);
+				pro->set_m_channelid(recv.m_channelid());
+
+				auto itor_channelid = m_chatitem.find(recv.m_channelid());
+				if (itor_channelid == m_chatitem.end())
+				{
+					send_client(adata.m_data->identifier(), pro);
+					return true;
+				}
+				for (pbnet::chatitem& item : itor_channelid->second)
+				{
+					*pro->add_m_chatlist() = item;
+				}
+
+				auto itor_update_channelid = m_update_chatitem.find(recv.m_channelid());
+				if (itor_update_channelid == m_update_chatitem.end())
+				{
+					send_client(adata.m_data->identifier(), pro);
+					return true;
+				}
+				for (pbnet::chatitem& item : itor_update_channelid->second)
+				{
+					*pro->add_m_chatlist() = item;
+				}
+
+				pro->set_m_stat(true);
+				send_client(adata.m_data->identifier(), pro);
+				return true;
+			}
+		return true;
+	}
 }// namespace ngl
