21478f75a0eb267f5f5c086b5fea560b5cdde159
NingLeiXueR <348634371@qq.com>
2024-03-27 11:21:16 +0800

    Update main.cpp

--- server/node/core/main.cpp
+++ server/node/core/main.cpp
@@ -13,57 +13,9 @@
 
 Dumper lDumper;
 
-struct abc
-{
-	int m_1;
-	float m_2;
-	std::string m_3;
-	std::vector<int32_t> m_4;
-	std::map<int32_t, float> m_5;
-
-	jsonfunc( "_1", m_1, "_2", m_2, "_3", m_3, "_4", m_4,"_5", m_5)
-};
 
 int main(int argc, char** argv)
 {	
-	ngl::ijson lijson;
-	std::vector<int32_t> lv;
-	for (int i = 0; i < 10; ++i)
-		lv.push_back(i);
-	lijson.write("test", lv);
-
-	abc ltemp;
-	ltemp.m_1 = 1989;
-	ltemp.m_2 = 123.3;
-	ltemp.m_3 = "chinachina";
-	ltemp.m_4.push_back(2);
-	ltemp.m_4.push_back(3);
-	ltemp.m_5.insert(std::make_pair(1, 1.1));
-	ltemp.m_5.insert(std::make_pair(2, 2.1));
-
-	ltemp.write(lijson, "kkk");
-
-	std::vector<abc> lvecabc;
-	lvecabc.push_back(ltemp);
-	ltemp.m_1 += 1000;
-	lvecabc.push_back(ltemp);
-	lijson.write("kkk2", lvecabc);
-
-	std::string ljson;
-	lijson.get(ljson);
-
-	std::cout << std::endl;
-
-	ngl::ojson lojson(ljson.c_str());
-	std::vector<int32_t> lv2;
-	lojson.read("test", lv2);
-	abc ltemp2;
-	ltemp2.read(lojson, "kkk");
-	std::vector<abc> lvecabc2;
-	lojson.read("kkk2", lvecabc2);
-	//cJSON_CreateArray*/
-
-
 	ngl::allcsv::load();
 	nconfig::init();
 	nconfig::load("config");
