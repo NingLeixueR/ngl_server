60ecea1794f1deaea671ec63e0fd1b0644df6fe2
NingLeiXueR <348634371@qq.com>
2024-05-20 11:50:10 +0800

    简单修改

--- public/csharp/csharp/csharp/xml/nconfig.cs
+++ public/csharp/csharp/csharp/xml/nconfig.cs
@@ -15,13 +15,11 @@ namespace ngl
     class NConfig
     {
         public static Int32 m_actor_delayed = 0;
-        public static Int32 m_logserverid = 0;
         public static bool m_varint = false;
         public static bool m_isxor = false;
-        public static Int32 m_waiting = 0;
+        public static Int32 m_sessionewait = 0;
         public static bool m_robot_test = false;
         public static string m_xor_str = "";
-        public static Int32 m_reloadcsv_version = 0;
         public static Dictionary<string, Int32> m_reloadcsv = new Dictionary<string, Int32>();
         public static string m_gmurl = "";
         public static string m_push_server_config = "";
@@ -69,20 +67,14 @@ namespace ngl
                 return;
 
             m_actor_delayed = XmlGetInt32(lAttributes, "actor_delayed");
-            m_logserverid = XmlGetInt32(lAttributes, "logserverid");
             if (XmlGetString(lAttributes, "varint") == "true")
                 m_varint = true;
             if (XmlGetString(lAttributes, "isxor") == "true")
                 m_isxor = true;
-            m_waiting = XmlGetInt32(lAttributes, "waiting");
+            m_sessionewait = XmlGetInt32(lAttributes, "sessionewait");
             if (XmlGetString(lAttributes, "robot_test") == "true")
                 m_robot_test = true;
             m_xor_str = XmlGetString(lAttributes, "xor_str");
-            m_reloadcsv_version = XmlGetInt32(lAttributes, "reloadcsv_version");
-            string lstr = XmlGetString(lAttributes, "reloadcsv");
-            string[] result = Regex.Split(lstr, "\\*");
-            foreach (var item in result)
-                m_reloadcsv.Add(item, 1);
             m_gmurl = XmlGetString(lAttributes, "gmurl");
             m_push_server_config = XmlGetString(lAttributes, "push_server_config");
             var lbytes = System.Text.Encoding.Default.GetBytes(m_xor_str);
