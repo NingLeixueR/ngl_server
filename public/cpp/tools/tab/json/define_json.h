3223131232c8b755b49efb40cc33e15adf7e3ea2
NingLeiXueR <348634371@qq.com>
2024-05-22 16:09:09 +0800

    Update define_json.h

--- public/cpp/tools/tab/json/define_json.h
+++ public/cpp/tools/tab/json/define_json.h
@@ -3,25 +3,25 @@
 #include "json_write.h"
 #include "json_read.h"
 
-#define jsonfunc(...)											\
+#define jsonfunc(...)												\
 	inline void write(ngl::json_write& ijsn, const char* akey)const	\
-	{															\
+	{																\
 		ngl::json_write ltemp;										\
-		write(ltemp);											\
-		ijsn.write(akey, ltemp.nofree());						\
-	}															\
+		write(ltemp);												\
+		ijsn.write(akey, ltemp.nofree());							\
+	}																\
 	inline void write(ngl::json_write& ijsn)const					\
-	{															\
-		ijsn.write(__VA_ARGS__);								\
-	}															\
+	{																\
+		ijsn.write(__VA_ARGS__);									\
+	}																\
 	inline bool read(ngl::json_read& ijsn, const char* akey)		\
-	{															\
+	{																\
 		ngl::json_read ltemp;										\
-		if (ijsn.read(akey, ltemp) == false)					\
-		return false;											\
-		return read(ltemp);										\
-	}															\
+		if (ijsn.read(akey, ltemp) == false)						\
+		return false;												\
+		return read(ltemp);											\
+	}																\
 	inline bool read(ngl::json_read& ijsn)							\
-	{															\
-		return ijsn.read(__VA_ARGS__);							\
+	{																\
+		return ijsn.read(__VA_ARGS__);								\
 	}
