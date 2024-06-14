c6c8dace7d51eaeb8caca6239484f5ad9dcfe651
NingLeiXueR <348634371@qq.com>
2024-06-14 11:15:53 +0800

    优化-规范化命名

--- public/cpp/protocol/protocol/protocoltools.cpp
+++ public/cpp/protocol/protocol/protocoltools.cpp
@@ -5,12 +5,22 @@
 
 namespace ngl
 {
-	bool protocoltools::names(char* abuff, int abuffsize, const char* aname, i32_protocolnum aprotocolnumber, EPROTOCOL_TYPE atype)
+	bool protocoltools::names(
+		char* abuff, 
+		int abuffsize, 
+		const char* aname, 
+		i32_protocolnum aprotocolnumber, 
+		EPROTOCOL_TYPE atype
+	)
 	{
 		return snprintf(abuff, 1024, "%d:%s:%d", atype, aname, aprotocolnumber) > 0;
 	}
 
-	void protocoltools::push(i32_protocolnum aprotocolnum, const std::string& aprotocolname, EPROTOCOL_TYPE atype)
+	void protocoltools::push(
+		i32_protocolnum aprotocolnum, 
+		const std::string& aprotocolname, 
+		EPROTOCOL_TYPE atype
+	)
 	{
 		char lbuff[1024] = { 0x0 };
 		if (names(lbuff, 1024, aprotocolname.c_str(), aprotocolnum, atype))
--- public/cpp/protocol/protocol/protocoltools.h
+++ public/cpp/protocol/protocol/protocoltools.h
@@ -12,8 +12,20 @@ namespace ngl
 	class protocoltools
 	{
 	public:
-		static bool names(char* abuff, int abuffsize, const char* aname, i32_protocolnum aprotocolnumber, EPROTOCOL_TYPE atype);
-		static void push(i32_protocolnum aprotocolnum, const std::string& aprotocolname, EPROTOCOL_TYPE atype);
+		static bool names(
+			char* abuff, 
+			int abuffsize, 
+			const char* aname, 
+			i32_protocolnum aprotocolnumber, 
+			EPROTOCOL_TYPE atype
+		);
+
+		static void push(
+			i32_protocolnum aprotocolnum, 
+			const std::string& aprotocolname, 
+			EPROTOCOL_TYPE atype
+		);
+
 		static const char* name(i32_protocolnum aprotocolnum, EPROTOCOL_TYPE atype);
 	};
 }// namespace ngl
