c45a30ec8f0f0d5d34b983c131aef75c221fb08e
NingLeiXueR <348634371@qq.com>
2024-06-19 09:41:05 +0800

    Update activitydb.cpp

--- public/cpp/logic/public/public/activity/activitydb.cpp
+++ public/cpp/logic/public/public/activity/activitydb.cpp
@@ -11,16 +11,13 @@ namespace ngl
 		(*lstream) << "actor_manage_activity###loaddb_finish" << std::endl;
 		for (std::pair<const nguid, data_modified<pbdb::db_activity>>& item : data())
 		{
-			(*lstream) << std::format("activityid:{}") << std::endl;
+			(*lstream) << std::format("activityid:{}", item.first) << std::endl;
 			actor_manage_activity* lactor = actor();
-			std::shared_ptr<activity> lactivity = activity::make(
-				item.second.getconst().m_calendarid(), 
-				item.first, 
-				ttab_calendar::data::time(
-					item.second.getconst().m_beg(), 
-					item.second.getconst().m_end()
-				), 
-				*this
+			const pbdb::db_activity& ldbactivity = item.second.getconst();
+			std::shared_ptr<activity> lactivity = activity::make(ldbactivity.m_calendarid()
+				, item.first
+				, ttab_calendar::data::time(ldbactivity.m_beg(), ldbactivity.m_end())
+				, *this
 			);
 			lactor->add_activity(item.first, lactivity);
 		}
