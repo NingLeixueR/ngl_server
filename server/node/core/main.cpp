2b00bfdde2e4d98a4b7e682bef25dceb6ee12ed3
NingLeiXueR <348634371@qq.com>
2024-08-06 16:06:33 +0800

    优化-规范化命名

--- server/node/core/main.cpp
+++ server/node/core/main.cpp
@@ -16,53 +16,6 @@
 
 Dumper lDumper;
 
-
-
-const std::string base64_chars =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-"abcdefghijklmnopqrstuvwxyz"
-"0123456789+/";
-
-std::string base64_encode(const std::string& data) {
-	std::string encoded;
-	int i = 0;
-	int j = 0;
-	unsigned char char_array_3[3];
-	unsigned char char_array_4[4];
-
-	for (const auto& c : data) {
-		char_array_3[i++] = c;
-		if (i == 3) {
-			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-			char_array_4[3] = char_array_3[2] & 0x3f;
-
-			for (i = 0; (i < 4); i++)
-				encoded += base64_chars[char_array_4[i]];
-			i = 0;
-		}
-	}
-
-	if (i) {
-		for (j = i; j < 3; j++)
-			char_array_3[j] = '\0';
-
-		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-
-		for (j = 0; (j < i + 1); j++)
-			encoded += base64_chars[char_array_4[j]];
-
-		while ((i++ < 3))
-			encoded += '=';
-
-	}
-
-	return encoded;
-}
-
 int main(int argc, char** argv)
 {
 	if (argc <= 1)
