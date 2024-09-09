0325a93d2dfc51947ac4f13577baf8f3f10036d3
NingLeiXueR <348634371@qq.com>
2024-09-09 19:04:53 +0800

    Update nprotocol_g2c.cpp

--- public/cpp/actor/nprotocol_g2c.cpp
+++ public/cpp/actor/nprotocol_g2c.cpp
@@ -33,7 +33,6 @@ namespace ngl
 			, pbnet::PROBUFF_NET_ADDFRIEND_RESPONSE
 			, pbnet::PROBUFF_NET_RATIFY_ADDFRIEND_RESPONSE
 			, pbnet::PROBUFF_NET_ERASEFRIEND_RESPONSE
-			>();
 		>();
 	}
 }//namespace ngl
