8380950d9012ba08738842faf306a910bc58c9f5
NingLeiXueR <348634371@qq.com>
2024-10-17 17:42:27 +0800

    Update sysconfig.h

--- public/cpp/tools/tab/xml/sysconfig.h
+++ public/cpp/tools/tab/xml/sysconfig.h
@@ -53,113 +53,29 @@ namespace ngl
 	public:
 		static void init();
 
-		static ELOGLEVEL loglevel()
-		{ 
-			return m_loglevel;
-		}
-	
-		static int32_t logline()
-		{
-			return m_logline;
-		}
-
-		static int32_t logflushtime()
-		{
-			return m_logflushtime;
-		}
-
-		static bool logiswrite()
-		{
-			return m_logiswrite;
-		}
-
-		static bool logconsole()
-		{
-			return m_logconsole;
-		}
-
-		static int32_t consumings()
-		{
-			return m_consumings;
-		}
-
-		static std::string& xorkey()
-		{
-			return m_xorkey;
-		}
-
-		static int32_t xorkeynum()
-		{
-			return m_xorkeynum;
-		}
-
-		static bool isxor()
-		{
-			return m_isxor;
-		}
-
-		static bool varint()
-		{
-			return m_varint;
-		}
-
-		static bool robot_test()
-		{
-			return m_robot_test;
-		}
-
-		static int32_t kcpping()
-		{
-			return m_kcpping;
-		}
-
-		static int32_t kcppinginterval()
-		{
-			return m_kcppinginterval;
-		}
-
-		static std::string& kcpsession()
-		{
-			return m_kcpsession;
-		}
-
-		static int32_t sessionwait()
-		{
-			return m_sessionewait;
-		}
-
-		static int32_t open_servertime()
-		{
-			return m_open_servertime;
-		}
-
-		static int32_t head_version()
-		{
-			return m_head_version;
-		}
-
-		static int32_t rate_interval()
-		{
-			return m_rate_interval;
-		}
+		static ELOGLEVEL loglevel()		{ return m_loglevel; }
+		static int32_t logline()		{ return m_logline; }
+		static int32_t logflushtime()	{ return m_logflushtime; }
+		static bool logiswrite()		{ return m_logiswrite; }
+		static bool logconsole()		{ return m_logconsole; }
+		static int32_t consumings()		{ return m_consumings; }
+		static std::string& xorkey()	{ return m_xorkey; }
+		static int32_t xorkeynum()		{ return m_xorkeynum; }
+		static bool isxor()				{ return m_isxor; }
+		static bool varint()			{ return m_varint; }
+		static bool robot_test()		{ return m_robot_test; }
+		static int32_t kcpping()		{ return m_kcpping; }
+		static int32_t kcppinginterval(){ return m_kcppinginterval; }
+		static std::string& kcpsession(){ return m_kcpsession; }
+		static int32_t sessionwait()	{ return m_sessionewait; }
+		static int32_t open_servertime(){ return m_open_servertime; }
+		static int32_t head_version()	{ return m_head_version; }
+		static int32_t rate_interval()	{ return m_rate_interval; }		
+		static int32_t rate_count()		{ return m_rate_count; }
+		static int32_t heart_beat_interval() { return m_heart_beat_interval; }
+		static int32_t net_timeout()	{ return m_net_timeout; }
 		
-		static int32_t rate_count()
-		{
-			return m_rate_count;
-		}
-
-		static int32_t heart_beat_interval()
-		{
-			return m_heart_beat_interval;
-		}
-
-		static int32_t net_timeout()
-		{
-			return m_net_timeout;
-		}
-
 		static void init_gatewayids();
-
 		static std::vector<i32_serverid>& gatewayids();
 	};
 }//namespace ngl
