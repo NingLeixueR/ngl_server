64c9c862845067cb10fabaea9ec056d8582938e2
NingLeiXueR <348634371@qq.com>
2024-10-25 18:49:55 +0800

    Update time_consuming.cpp

--- public/cpp/tools/time_consuming.cpp
+++ public/cpp/tools/time_consuming.cpp
@@ -11,10 +11,16 @@ namespace ngl
 
 	time_consuming::~time_consuming()
 	{
-		int64_t lconsuming = time_wheel::getms() - m_beg;
-		bool lerror = lconsuming > sysconfig::consumings();
-		(lerror ? log_info() : log_error())->print("time consuming [{}] [{}]",
-			m_name, lconsuming
-		);
+		try
+		{
+			int64_t lconsuming = time_wheel::getms() - m_beg;
+			bool lerror = lconsuming > sysconfig::consumings();
+			(lerror ? log_info() : log_error())->print("time consuming [{}] [{}]",
+				m_name, lconsuming
+			);
+		}
+		catch (...)
+		{
+		}		
 	}
 }
