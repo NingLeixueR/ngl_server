a5ce5b31f81f69d5cc310ff6b21c8368fde72b15
NingLeiXueR <348634371@qq.com>
2024-11-13 14:51:09 +0800

    优化-规范化命名
    优化-规范化命名

--- server/idl/core/ub_2.cpp
+++ server/idl/core/ub_2.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 24-10-25 10:34:12
+// 创建时间 24-11-13 14:16:21
 #include "actor.cpp"
 #include "unit.cpp"
 #include "ukcp.cpp"
--- server/node/core/ub_2.cpp
+++ server/node/core/ub_2.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 24-10-25 10:33:01
+// 创建时间 24-11-13 14:15:44
 #include "actor.cpp"
 #include "unit.cpp"
 #include "ukcp.cpp"
