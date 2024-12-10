9b0e9a2318cdf5a1ba96d4069ee7ded37de6cf64
NingLeiXueR <348634371@qq.com>
2024-12-10 16:05:48 +0800

    优化-规范化命名
    优化-规范化命名

--- server/idl/core/ub_1.cpp
+++ server/idl/core/ub_1.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 24-11-19 16:36:19
+// 创建时间 24-12-10 15:57:49
 #include "actor_manage_activity.cpp"
 #include "actor_manage_plays.cpp"
 #include "calendar_function.cpp"
