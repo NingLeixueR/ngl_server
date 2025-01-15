d538b7b7a0e46bb9fb889514337a87f8df5679cf
NingLeiXueR <348634371@qq.com>
2025-01-15 15:55:18 +0800

    优化-规范化命名
    优化-规范化命名

--- public/cpp/logic/public/public/ranklist/ranklist.h
+++ public/cpp/logic/public/public/ranklist/ranklist.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "nactor_auto.h"
+#include "rank_item.h"
 #include "db.pb.h"
 
 #include <iostream>
@@ -10,90 +11,6 @@
 namespace ngl
 {
 	class actor_ranklist;
-
-	struct rank_item
-	{
-		i64_actorid m_actorid;
-		int64_t		m_values[pbdb::eranklist::count];
-		int32_t		m_time[pbdb::eranklist::count];
-
-		rank_item() :
-			m_actorid(0),
-			m_values{0},
-			m_time{0}
-		{}
-
-		const pbdb::db_brief* get()const
-		{
-			return tdb_brief::nsp_cli<actor_ranklist>::getconst(m_actorid);
-		}
-
-		void init(const pbdb::db_brief& abrief, const pbdb::db_ranklist* aranklist, pbdb::eranklist atype, const std::function<int64_t(const pbdb::db_brief&)>& avalfun)
-		{
-			m_values[atype] = avalfun(abrief);
-
-			if (aranklist != nullptr)
-			{
-				auto& lmap = aranklist->m_items();
-				auto itor = lmap.find((int32_t)atype);
-				if (itor != lmap.end())
-				{
-					m_time[atype] = itor->second.m_time();
-					return;
-				}
-			}
-			m_time[atype] = (int32_t)localtime::gettime();
-		}
-
-		void init(const pbdb::db_brief& abrief, const pbdb::db_ranklist* aranklist)
-		{
-			m_actorid = abrief.m_id();
-			init(abrief, aranklist, pbdb::eranklist::lv, [](const pbdb::db_brief& abrief)
-				{
-					return  abrief.m_lv();
-				});
-		}
-
-		void change(pbdb::eranklist atype, pbdb::db_ranklist& aranklist)
-		{
-			pbdb::rankitem& ltemp = (*aranklist.mutable_m_items())[atype];
-			ltemp.set_m_time(m_time[atype]);
-			ltemp.set_m_value(m_values[atype]);
-		}
-		
-		bool bigger(pbdb::eranklist atype, const rank_item& ar)
-		{
-			if (m_values[atype] > ar.m_values[atype])
-			{
-				return true;
-			}
-			else if (m_values[atype] < ar.m_values[atype])
-			{
-				return false;
-			}
-			else
-			{
-				if (m_time[atype] < ar.m_time[atype])
-				{
-					return true;
-				}
-				else if (m_time[atype] > ar.m_time[atype])
-				{
-					return false;
-				}
-				else
-				{
-					return m_actorid < ar.m_actorid;
-				}
-			}
-		}
-
-		bool equal_value(pbdb::eranklist atype, const rank_item& ar)
-		{
-			return m_values[atype] == ar.m_values[atype];
-		}
-
-	};
 
 	template <pbdb::eranklist RTYPE>
 	class operator_value
@@ -113,15 +30,14 @@ namespace ngl
 
 		std::map<i64_actorid, rank_item> m_data;
 
-		class rank_set_base
+		class rankset_base
 		{
 			int32_t m_count;
 		public:
-			rank_set_base() :
+			rankset_base() :
 				m_count(-1)
 			{}
 
-			virtual const rank_item* find(rank_item* aitem) = 0;
 			virtual void erase(rank_item* aitem) = 0;
 			virtual void insert(rank_item* aitem) = 0;
 			virtual void foreach(const std::function<void(int32_t, const rank_item*)>&) = 0;
@@ -138,20 +54,10 @@ namespace ngl
 		};
 
 		template <pbdb::eranklist ETYPE>
-		class rank_set :public rank_set_base
+		class rankset :public rankset_base
 		{
 			std::set<rank_item*, operator_value<ETYPE>> m_rankdata;
 		public:
-			virtual const rank_item* find(rank_item* aitem)
-			{
-				auto itor = m_rankdata.find(aitem);
-				if (itor == m_rankdata.end())
-				{
-					return nullptr;
-				}
-				return *itor;
-			}
-
 			virtual void erase(rank_item* aitem)
 			{
 				m_rankdata.erase(aitem);
@@ -202,11 +108,11 @@ namespace ngl
 			}
 		};
 
-		std::unique_ptr<rank_set_base> m_ranks[pbdb::eranklist::count];
+		std::unique_ptr<rankset_base> m_ranks[pbdb::eranklist::count];
 	public:
 		ranklist()
 		{
-			m_ranks[pbdb::eranklist::lv] = std::make_unique<rank_set<pbdb::eranklist::lv>>();
+			m_ranks[pbdb::eranklist::lv] = std::make_unique<rankset<pbdb::eranklist::lv>>();
 			m_ranks[pbdb::eranklist::lv]->set_count(10);
 		}
 
@@ -309,7 +215,6 @@ namespace ngl
 						}
 					}
 				}
-				
 			}
 			return true;
 		}
@@ -327,7 +232,7 @@ namespace ngl
 
 			for (int32_t i = 0; i < pbdb::eranklist::count; ++i)
 			{
-				rank_set_base& lrank = *m_ranks[i].get();
+				rankset_base& lrank = *m_ranks[i].get();
 				lrank.insert(&ltempitem);
 				m_ranks[i]->check();
 			}
@@ -336,7 +241,7 @@ namespace ngl
 		virtual void initdata()
 		{
 			log_error()->print("actor_ranklist###loaddb_finish");
-			rank_set_base& lrank = *m_ranks[pbdb::eranklist::lv].get();
+			rankset_base& lrank = *m_ranks[pbdb::eranklist::lv].get();
 
 			for (const std::pair<const nguid, data_modified<pbdb::db_ranklist>>& item : data())
 			{
