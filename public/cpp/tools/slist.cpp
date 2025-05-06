2e16763a3b1ba0a081ed92f9165e04b5c1ed5c72
NingLeiXueR <348634371@qq.com>
2025-05-06 17:36:59 +0800

    替换生产/消费队列

---
+++ public/cpp/tools/slist.cpp
@@ -0,0 +1,56 @@
+#include "slist.h"
+
+
+namespace ngl
+{
+	void test_slist()
+	{
+		// # 测试 两个队列+ 800个元素  再释放一半  重复1w次 测试stl 与  ngl::slist
+		int64_t lbeg = ngl::time_wheel::getms();
+		std::list<int> lstl1;
+		std::list<int> lstl2;
+		for (int i = 0; i < 10000; ++i)
+		{
+			for (int j = 0; j < 400; ++j)
+			{
+				lstl1.push_back(j);
+			}
+			lstl1.swap(lstl2);
+			for (int j = 0; j < 400; ++j)
+			{
+				lstl1.push_back(j);
+			}
+			for (int j = 0; j < 200; ++j)
+			{
+				lstl2.pop_front();
+			}
+			lstl1.insert(lstl1.begin(), lstl2.begin(), lstl2.end());
+			lstl1.clear();
+		}
+		int64_t lconsuming = ngl::time_wheel::getms() - lbeg;
+		std::cout << "stl:" << lconsuming << std::endl;
+		lbeg = ngl::time_wheel::getms();
+		ngl::slist_production<int>  ls1;
+		ngl::slist_consumption<int> ls2;
+		for (int i = 0; i < 10000; ++i)
+		{
+			for (int j = 0; j < 400; ++j)
+			{
+				ls1.push_back(j);
+			}
+			ls1.swap(ls2);
+			for (int j = 0; j < 400; ++j)
+			{
+				ls1.push_back(j);
+			}
+			for (int j = 0; j < 200; ++j)
+			{
+				ls2.pop_front();
+			}
+			ls1.push_front(ls2);
+			ls1.clear();
+		}
+		lconsuming = ngl::time_wheel::getms() - lbeg;
+		std::cout << "slist:" << lconsuming << std::endl;
+	}
+}//namespace ngl
--- server/makeproto/core/ub_1.cpp
+++ server/makeproto/core/ub_1.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 25-05-06 14:20:37
+// 创建时间 25-05-06 17:32:55
 #include "calendar_function.cpp"
 #include "attribute_value.cpp"
 #include "ttab_specialid.cpp"
@@ -69,6 +69,7 @@
 #include "gdbdmp.cpp"
 #include "family.cpp"
 #include "tools.cpp"
+#include "slist.cpp"
 #include "nrfun.cpp"
 #include "nguid.cpp"
 #include "mdump.cpp"
