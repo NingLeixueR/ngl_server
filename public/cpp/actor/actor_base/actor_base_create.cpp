ddcdf89a7c65e5c2335087656f6ecc3654fc4523
NingLeiXueR <348634371@qq.com>
2025-05-08 17:43:27 +0800

    使用模板匿名函数统一逻辑

--- public/cpp/actor/actor_base/actor_base_create.cpp
+++ public/cpp/actor/actor_base/actor_base_create.cpp
@@ -17,46 +17,44 @@ namespace ngl
 			return nullptr;
 		}
 		std::shared_ptr<actor_base> lpactor_base = nullptr;
-		auto linitfun = [] <ENUM_ACTOR ACTORTYPE, typename T>(std::shared_ptr<actor_base>& apactor)
+		auto linitfun = [] <ENUM_ACTOR ACTORTYPE, typename TActorDerived>(std::shared_ptr<actor_base>& apactor)
 		{
-			((T*)(apactor.get()))->init_rfun<T>();
-			actor_base::first_nregister<T>(ACTORTYPE);
+			((TActorDerived*)(apactor.get()))->init_rfun<TActorDerived>();
+			actor_base::first_nregister<TActorDerived>(ACTORTYPE);
 		};
 
 		switch (atype)
 		{
-		case ACTOR_ROLE:
-		{
-			lpactor_base = std::make_shared<actor_role>(tab_self_area, aid, aparm);
-			linitfun.operator()<ACTOR_ROLE, actor_role>(lpactor_base);
-		}
-		break;
-		case ACTOR_ROBOT:
-		{
-			lpactor_base = std::make_shared<actor_robot>(tab_self_area, aid, aparm);
-			linitfun.operator()<ACTOR_ROBOT, actor_robot>(lpactor_base);
-		}
-		break;
-		case ACTOR_LOG:
-		{
-			lpactor_base = std::make_shared<actor_log>(aid);
-			linitfun.operator()<ACTOR_LOG, actor_log>(lpactor_base);
-		}
-		break;
-		case ACTOR_EXAMPLE_GUESS_NUMBER:
-		{
-			auto lroleids = (const std::map<int32_t, i64_actorid>*)aparm;
-			lpactor_base = std::make_shared<actor_example_guess_number>(*lroleids, aid);
-			linitfun.operator()<ACTOR_EXAMPLE_GUESS_NUMBER, actor_example_guess_number>(lpactor_base);
-		}
-		break;
-		default:
+			case ACTOR_ROLE:
+			{
+				lpactor_base = std::make_shared<actor_role>(tab_self_area, aid, aparm);
+				linitfun.operator() < ACTOR_ROLE, actor_role > (lpactor_base);
+			}
 			break;
-		}
-		if (lpactor_base == nullptr)
-		{
-			ngl::log_error()->print("actor_base::create({},{})", em<ENUM_ACTOR>::get_name(atype), aid);
-			return nullptr;
+			case ACTOR_ROBOT:
+			{
+				lpactor_base = std::make_shared<actor_robot>(tab_self_area, aid, aparm);
+				linitfun.operator() < ACTOR_ROBOT, actor_robot > (lpactor_base);
+			}
+			break;
+			case ACTOR_LOG:
+			{
+				lpactor_base = std::make_shared<actor_log>(aid);
+				linitfun.operator() < ACTOR_LOG, actor_log > (lpactor_base);
+			}
+			break;
+			case ACTOR_EXAMPLE_GUESS_NUMBER:
+			{
+				auto lroleids = (const std::map<int32_t, i64_actorid>*)aparm;
+				lpactor_base = std::make_shared<actor_example_guess_number>(*lroleids, aid);
+				linitfun.operator() < ACTOR_EXAMPLE_GUESS_NUMBER, actor_example_guess_number > (lpactor_base);
+			}
+			break;
+			default:
+			{
+				ngl::log_error()->print("actor_base::create({},{})", em<ENUM_ACTOR>::get_name(atype), aid);
+				return nullptr;
+			}
 		}
 		actor_manage::getInstance().add_actor(lpactor_base, [lpactor_base]() 
 			{
