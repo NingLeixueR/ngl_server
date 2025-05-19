d0cabd7923486eb69635a7f873c472cff2ca545e
NingLeiXueR <348634371@qq.com>
2025-05-19 19:08:13 +0800

    Update actor_ranklist_handle.cpp

--- public/cpp/actor/actor_logic/actor_ranklist/message/actor_ranklist_handle.cpp
+++ public/cpp/actor/actor_logic/actor_ranklist/message/actor_ranklist_handle.cpp
@@ -45,7 +45,7 @@ namespace ngl
 	bool actor_ranklist::handle(const message<mforward<pbnet::PROBUFF_NET_RANKLIST>>& adata)
 	{
 		auto lrecv = adata.get_data()->data();
-		m_ranklist.sync_ranklist(adata.get_data()->identifier(), lrecv->m_type(), lrecv->m_page(), lrecv->m_everynum());
+		m_ranklist.sync_ranklist(adata.get_data()->identifier(), lrecv->m_type(), lrecv->m_page());
 		return true;
 	}
 }//namespace ngl
