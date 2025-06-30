65a300742ec030de0361b87d9810da40f7147c1d
NingLeiXueR <348634371@qq.com>
2025-06-30 23:23:13 +0800

    db buff支持尽量多的数据

--- public/cpp/tools/db/mysql/db_buff.h
+++ public/cpp/tools/db/mysql/db_buff.h
@@ -38,39 +38,81 @@ namespace ngl
 			e_buffsize = 10 * 1024 * 1024, // µ•blob…œœﬁ 10M
 		};
 		std::shared_ptr<dbuff> m_buff;
+		std::shared_ptr<dbuff> m_mallocbuff;
 	public:
 		inline db_buff() :
 			m_buff(std::make_shared<dbuff>(e_buffsize))
+			, m_mallocbuff(nullptr)
 		{}
 
 		inline char* buff()
 		{
+			if (m_mallocbuff != nullptr)
+			{
+				return m_mallocbuff->m_buff;
+			}
 			return m_buff->m_buff;
 		}
 
 		inline int32_t buffsize()
 		{
+			if (m_mallocbuff != nullptr)
+			{
+				return m_mallocbuff->m_buffsize;
+			}
 			return m_buff->m_buffsize;
 		}
 
 		inline int32_t pos()
 		{
+			if (m_mallocbuff != nullptr)
+			{
+				return m_mallocbuff->m_pos;
+			}
 			return m_buff->m_pos;
 		}
 
 		inline void reset()
 		{
+			if (m_mallocbuff != nullptr)
+			{
+				m_mallocbuff = nullptr;
+			}
 			m_buff->m_pos = 0;
 		}
 
 		template <typename T>
-		inline void serialize(T& adata)
+		inline bool do_serialize(T& adata, std::shared_ptr<dbuff>& abuff)
 		{
 			ngl::serialize lserialize(m_buff->m_buff, m_buff->m_buffsize);
 			if (lserialize.push(adata))
 			{
 				m_buff->m_pos = lserialize.byte();
+				return true;
+			}
+			return false;
+		}
+
+		template <typename T>
+		inline void serialize(T& adata)
+		{
+			if (do_serialize(adata, m_buff))
+			{
 				return;
+			}
+
+			if (adata.m_isbinary)
+			{
+				int32_t lbytes = adata.m_data->ByteSize();
+				if (lbytes > e_buffsize)
+				{
+					m_mallocbuff = std::make_shared<dbuff>(lbytes);
+					if (do_serialize(adata, m_mallocbuff))
+					{
+						return;
+					}
+					reset();
+				}
 			}
 			Throw("malloc_buff.serialize fail");
 		}
