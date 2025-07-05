2a03254de9b93ae4b3187114018d94b439eb1c8d
NingLeiXueR <348634371@qq.com>
2025-07-05 10:02:17 +0800

    lua脚本提交

--- bin/script/lua/rfunction.lua
+++ bin/script/lua/rfunction.lua
@@ -1,9 +1,13 @@
 local dbdata = require("dbdata").getInstance()
 
-function push_data(adbname, adatajson)
-    dbdata:push_data(adbname, adatajson)
+function push_data(adbname, aactorid, adatajson)
+    dbdata:push_data(adbname, aactorid, adatajson)
 end
 
-function check_outdata(adbname)
-    return dbdata:check_outdata(adbname)
+function check_outdata(adbname, aactorid)
+    return dbdata:check_outdata(adbname, aactorid)
+end
+
+function check_outdata_all(adbname)
+    return dbdata:check_outdata_all(adbname)
 end
