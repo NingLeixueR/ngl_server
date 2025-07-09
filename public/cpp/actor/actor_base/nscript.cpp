853115e48a68ccd1446157be59b8fbb256b6dcb0
NingLeiXueR <348634371@qq.com>
2025-07-09 15:46:38 +0800

    lua脚本提交
    lua脚本提交

--- public/cpp/actor/actor_base/nscript.cpp
+++ public/cpp/actor/actor_base/nscript.cpp
@@ -5,8 +5,15 @@ extern "C"
 {
 	int send_client(lua_State* L)
 	{
+		//actor_type#areaid#dataid
 		const char* nguidstr = luaL_checkstring(L, 1);
-		ngl::i64_actorid lactorid = ngl::tools::lexical_cast<ngl::i64_actorid>(nguidstr);
+		ngl::ENUM_ACTOR lactortype;
+		std::string lactortypestr;
+		ngl::i16_area larea;
+		ngl::i32_actordataid ldataid;
+		ngl::tools::splite(nguidstr, "#", lactortypestr, larea, ldataid);
+		lactortype = ngl::em<ngl::ENUM_ACTOR>::get_enum(lactortypestr.c_str());
+		ngl::i64_actorid lactorid = ngl::nguid::make(lactortype, larea, ldataid);
 
 		const char* lmsgname = luaL_checkstring(L, 2);
 		const char* ljsonmsg = luaL_checkstring(L, 3);
