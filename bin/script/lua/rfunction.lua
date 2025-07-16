4fff0a280bf64568af3a1de1128247fd4984520c
NingLeiXueR <348634371@qq.com>
2025-07-16 19:26:50 +0800

    lua脚本的修改

--- bin/script/lua/rfunction.lua
+++ bin/script/lua/rfunction.lua
@@ -1,13 +1,17 @@
-local dbdata = require("dbdata").getInstance()
+local ngldata = require("ngldata").getInstance()
 
+-- 将CPP数据压入数据lua
+-- 1、数据库数据，配合check_outdata与check_outdata_all
+-- 2、Csv表数据
+-- 3、Config数据
 function push_data(adbname, aactorid, adatajson)
-    dbdata:push_data(adbname, aactorid, adatajson)
+    ngldata:push_data(adbname, aactorid, adatajson)
 end
 
 function check_outdata(adbname, aactorid)
-    return dbdata:check_outdata(adbname, aactorid)
+    return ngldata:check_outdata(adbname, aactorid)
 end
 
 function check_outdata_all(adbname)
-    return dbdata:check_outdata_all(adbname)
+    return ngldata:check_outdata_all(adbname)
 end
