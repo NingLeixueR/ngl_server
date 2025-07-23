8268b82179273d6b938364b0cdce43c807c3dec3
NingLeiXueR <348634371@qq.com>
2025-07-23 19:52:58 +0800

    完善lua与c++交互

--- bin/script/lua/test.lua
+++ bin/script/lua/test.lua
@@ -1,6 +1,18 @@
 local ngldata = require("ngldata").getInstance()
 require("ngldata")
 
+function print_table(t)
+    if type(t) ~= "table" then
+        print(t)
+        return
+    end
+    print("{")
+    for k, v in pairs(t) do
+        print("  " .. tostring(k) .. ": " .. tostring(v))
+    end
+    print("}")
+end
+
 function handle(amsgname, amsgjson)
 	-- 如何获取db数据:明确需求是否修改
 	-- 无需修改数据使用 ngldata:getconst("数据名称")
@@ -10,27 +22,32 @@ function handle(amsgname, amsgjson)
 	-- nguid:actor_type#areaid#dataid
 	--send_client("guid", "msgname", "msgjson")
 	--send_actor("guid", "msgname", "msgjson")
-
+	
+	-- 因为 tab_servers 是只读的 所以不允许玩家修改，无法通过get获取
+	print("ngldata:get ############################")
 	local tab = ngldata:get("tab_servers","1")
 	if tab ~= nil then
 		ngldata:print_table(tab)
 	end
-	--ngldata:get("kkk","777")
+	print("############################")
+	print("ngldata:getconst ############################")
+	local tabconst = ngldata:getconst("tab_servers","1")
+	if tabconst ~= nil then
+		print_table(tabconst)
+		print("tabconst.m_threadnum = "..tabconst.m_threadnum)
+	end
+	print("############################")
 
 	-- 可以通过[amsgname]和[amsgjson]进行消息处理
 	print("msg name:" .. amsgname)
 	print(amsgjson)
 
-	
-
 	if amsgname == "np_actormodule_forward<PROBUFF_NET_TESTLUA>" then
+		-- 此处对db_testlua数据进行了修改
 		local testlua = ngldata:get("db_testlua", nguidstr2int64str("actor_testlua#1#1"))
-		--ngldata:print_table(testlua)
 		if testlua ~= nil then
 			ngldata:print_table(testlua["mvalue"])
 			table.remove(testlua["mvalue"], 1)
-			table.remove(testlua["mvalue"], 3)
-			table.remove(testlua["mvalue"], 5)
 			ngldata:print_table(testlua["mvalue"])
 		end
 	elseif amsgname == "np_testlua" then
