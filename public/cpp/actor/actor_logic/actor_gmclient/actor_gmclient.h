d5aa9a4e16557cf9d695ff1660d8d50a2a8691ab
NingLeiXueR <348634371@qq.com>
2025-09-15 09:57:49 +0800

    Update actor_gmclient.h

--- public/cpp/actor/actor_logic/actor_gmclient/actor_gmclient.h
+++ public/cpp/actor/actor_logic/actor_gmclient/actor_gmclient.h
@@ -22,33 +22,7 @@ namespace ngl
 	{
 		std::map<i32_protocolnum, std::string> m_data;
 
-		static std::vector<const char*>& parms(const char* astr = nullptr) {
-			static std::vector<const char*> tempvec; if (astr == nullptr) {
-				return tempvec;
-			} static std::string tempstr(astr); static std::atomic<bool> lregister = true; if (lregister.exchange(false) && !tempstr.empty()) {
-				tempvec = tools::split_str(&tempstr[0], tempstr.size());
-			} return tempvec;
-		} inline void json_write(ngl::njson_write& ijsn, const char* akey) {
-			ngl::njson_write ltemp; json_write(ltemp); ngl::njson::write(ijsn, akey, ltemp.nofree());
-		} inline void json_write(ngl::njson_write& ijsn) {
-			help_writejson ltemp(parms("m_data"), ijsn); ltemp.fun(0, m_data);
-		} inline bool json_read(ngl::njson_read& ijsn, const char* akey) {
-			ngl::njson_read ltemp; if (!ngl::njson::read(ijsn, akey, ltemp.json())) {
-				return false;
-			} return json_read(ltemp);
-		} inline bool json_read(ngl::njson_read& ijsn) {
-			help_readjson ltemp(parms("m_data"), ijsn); return ltemp.fun(0, m_data);
-		} bool push_format(ngl::ser::serialize_push* aserialize)const {
-			return ngl::ser::nserialize::push(aserialize, m_data);
-		} bool pop_format(ngl::ser::serialize_pop* aserialize) {
-			return ngl::ser::nserialize::pop(aserialize, m_data);
-		} void bytes_format(ngl::ser::serialize_byte* aserialize)const {
-			ngl::ser::nserialize::bytes(aserialize, m_data);
-		} void nlua_push(lua_State* aL, const char* aname = nullptr)const {
-			help_nlua<false> ltemp(aL, aname, parms("m_data")); ltemp.push(m_data);
-		} bool nlua_pop(lua_State* aL, const char* aname = nullptr) {
-			help_nlua<true> ltemp(aL, aname, parms("m_data")); return ltemp.pop(m_data);
-		}
+		dprotocol(protocols, m_data)
 	};
 
 	class actor_gmclient : 
