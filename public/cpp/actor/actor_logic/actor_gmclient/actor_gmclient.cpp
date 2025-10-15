3936786b3b4fc093ecf794c92fb3ea1327296d34
NingLeiXueR <348634371@qq.com>
2025-10-15 18:18:41 +0800

    tab_server配置优化
    tab_server配置优化

--- public/cpp/actor/actor_logic/actor_gmclient/actor_gmclient.cpp
+++ public/cpp/actor/actor_logic/actor_gmclient/actor_gmclient.cpp
@@ -10,7 +10,7 @@ namespace ngl
 				{
 					.m_type = ACTOR_GMCLIENT,
 					.m_area = tab_self_area,
-					.m_id = nconfig::m_tid,
+					.m_id = nconfig::m_nodeid,
 				},
 				.m_weight = 0x7fffffff,
 			})
@@ -24,7 +24,7 @@ namespace ngl
 
 	i64_actorid actor_gmclient::actorid(i16_area area)
 	{
-		return nguid::make(actor_type(), area, nconfig::m_tid);
+		return nguid::make(actor_type(), area, nconfig::m_nodeid);
 	}
 
 	void actor_gmclient::init()
