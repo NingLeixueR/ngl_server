c13270d9bc06bf5a549c3656b4c75f2d41b1dc09
NingLeiXueR <348634371@qq.com>
2025-10-15 17:01:30 +0800

    去除测试代码

--- server/node/core/main.cpp
+++ server/node/core/main.cpp
@@ -4,9 +4,5 @@
 
 int main(int argc, char** argv)
 {
-	int16_t lnumb = 1;
-	std::map<int32_t, bool> lmap = { {1,true},{2,true},{3,true} };
-	auto itor = lmap.find(lnumb);
-
 	return ngl_main(argc, argv);
 }
