41d0445bf842f4a9331bc513401543d05d42e1ae
NingLeiXueR <348634371@qq.com>
2025-10-28 11:25:26 +0800

    配置修改

--- bin/bat/win_start_client_1.bat
+++
@@ -1,4 +0,0 @@
-start node_robot robot 1 1 libo 1 1
-#start node_robot robot 1 1 wac 1 1
-#start node_robot robot 1 1 libo 1 1999
-#start node_robot robot 1 1 wac 1 1999
--- bin/bat/win_start_server_cross.bat
+++ bin/bat/win_start_server_cross.bat
@@ -1,2 +1,3 @@
 #¿ç·þ
 start node_cross cross -100 1
+start node_crossdb crossdb -100 1
