25e201b039f61c4c8e4f45c96524bc2b7e895648
NingLeiXueR <348634371@qq.com>
2025-10-28 11:25:26 +0800

    配置修改

---
+++ bin/bat/win_start_server.bat
@@ -0,0 +1,15 @@
+#一区
+set /P area=区服id
+start node_actor actor %rebot_value1% 1
+start node_game game %rebot_value1% 1
+#start node_game game %rebot_value1% 2
+start node_db db %rebot_value1% 1
+start node_login login  %rebot_value1% 1
+start node_log log  %rebot_value1% 1
+start node_gateway gateway %rebot_value1% 1
+#start node_gateway gateway %rebot_value1% 2
+start node_world world %rebot_value1% 1
+
+
+
+
