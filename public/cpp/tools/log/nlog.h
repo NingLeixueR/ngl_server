cf8a25e532ef853b4e1c3b361dd24119238c0d20
NingLeiXueR <348634371@qq.com>
2025-10-30 19:10:01 +0800

    增加文件头

--- public/cpp/tools/log/nlog.h
+++ public/cpp/tools/log/nlog.h
@@ -1,3 +1,16 @@
+/*
+* Copyright (c) [2020-2025] NingLeixueR
+* 
+* 项目名称：ngl_server
+* 项目地址：https://github.com/NingLeixueR/ngl_server
+* 
+* 本文件是 ngl_server 项目的一部分，遵循 MIT 开源协议发布。
+* 您可以按照协议规定自由使用、修改和分发本项目，包括商业用途，
+* 但需保留原始版权和许可声明。
+* 
+* 许可详情参见项目根目录下的 LICENSE 文件：
+* https://github.com/NingLeixueR/ngl_server/blob/main/LICENSE
+*/
 #pragma once
 
 #include "localtime.h"
--- server/idl/core/ub_1.cpp
+++ server/idl/core/ub_1.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 25-10-30 16:36:07
+// 创建时间 25-10-30 19:08:26
 #include "pb_field.cpp"
 #include "session_endpoint.cpp"
 #include "attribute_value.cpp"
