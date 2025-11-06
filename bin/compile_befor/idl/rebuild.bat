566041785073b171561525113ebd5d8fcbd8920c
NingLeiXueR <348634371@qq.com>
2025-11-06 17:51:22 +0800

    自动生成文件
    自动生成文件

--- bin/compile_befor/idl/rebuild.bat
+++ bin/compile_befor/idl/rebuild.bat
@@ -1,4 +1,4 @@
 cd ../../Debug/
-rebuild.exe ../../../server/libnglcore/core/ub ../../public/cpp 
-rebuild.exe ../../../server/makeproto/core/ub ../../public/cpp 
-rebuild.exe ../../../server/idl/core/ub ../../public/cpp 
+rebuild.exe ../../server/libnglcore/core/ub ../../public/cpp 
+rebuild.exe ../../server/makeproto/core/ub ../../public/cpp 
+rebuild.exe ../../server/idl/core/ub ../../public/cpp 
--- server/idl/core/ub_1.cpp
+++ server/idl/core/ub_1.cpp
@@ -1,5 +1,5 @@
 // 注意【rebuild.bat 工具生成文件，不要手动修改】
-// 创建时间 25-11-05 11:50:50
+// 创建时间 25-11-06 17:51:07
 #include "pb_field.cpp"
 #include "session_endpoint.cpp"
 #include "attribute_value.cpp"
