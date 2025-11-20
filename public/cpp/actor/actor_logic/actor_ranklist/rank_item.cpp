8e262358668e7ffc0b71eabe4274a3c747b74bbc
NingLeiXueR <348634371@qq.com>
2025-11-20 16:07:18 +0800

    重构nsp

--- public/cpp/actor/actor_logic/actor_ranklist/rank_item.cpp
+++ public/cpp/actor/actor_logic/actor_ranklist/rank_item.cpp
@@ -18,12 +18,7 @@ namespace ngl
 {
 	const pbdb::db_brief* rank_item::get()const
 	{
-		static tdb_brief::nsp_cread<actor_ranklist>* lpdb = nsp_instance<tdb_brief::nsp_cread<actor_ranklist>>::nclient(actor_ranklist::actorid());
-		if (lpdb == nullptr)
-		{
-			tools::no_core_dump();
-			return nullptr;
-		}
-		return lpdb->getconst(m_actorid);
+		static auto linstance = tdb_brief::nsp_cwrite<actor_role>::instance(actor_ranklist::actorid());
+		return linstance.getconst(m_actorid);
 	}
 }//namespace ngl
