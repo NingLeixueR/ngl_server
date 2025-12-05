86a1196109e1b29c8cb92e33ea4188cd10844e14
NingLeiXueR <348634371@qq.com>
2025-12-05 16:22:33 +0800

    对输入命令进行格式化处理

--- public/cpp/tools/tools.h
+++ public/cpp/tools/tools.h
@@ -877,6 +877,17 @@ namespace ngl
 		{
 			return check_cjson_and(atype, acjsontype) && check_cjson_and(atype, args...);
 		}
+
+		//É¾³ıÖØ¸´¶àÓàµÄach
+		static void erase_repeat(std::string& astrbuff, const char ach)
+		{
+			auto last = std::unique(astrbuff.begin(), astrbuff.end(),
+				[ach](char al, char ar) {
+					return al == ach && ar == ach;
+				});
+			astrbuff.erase(last, astrbuff.end());
+		}
+
 	};
 
 	template <typename T>
