81ce7d2655902fdd4b7a4202a25636d87dda6894
NingLeiXueR <348634371@qq.com>
2026-02-26 12:59:26 +0800

    删除第三方库 改用conan

--- public/cpp/third_party/libpg/include/libxslt/transformInternals.h
+++
@@ -1,9 +0,0 @@
-/*
- * Summary: set of internal interfaces for the XSLT engine transformation part.
- *
- * Copy: See Copyright for the status of this software.
- *
- * Author: David Kilzer <ddkilzer@apple.com>
- */
-
-void xsltCleanupSourceDoc(xmlDocPtr doc);
