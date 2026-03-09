7d45308ea90cbc0e3b80022982237f3894ec9bca
NingLeiXueR <348634371@qq.com>
2026-03-09 11:31:14 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ conan profile detect --force
 #-c "tools.files.download:verify=False" 设置不进行ssl验证(确保网络环境安全)
 #conan install . --output-folder=build/Release --build=missing -s build_type=Release -c "tools.files.download:verify=False" -s compiler.cppstd=20
 #conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20
+#conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20 -o abseil/*:shared=False -o protobuf/*:shared=False
+
 
 conan install . -s build_type=Release -s compiler.cppstd=20
 conan install . -s build_type=Debug -s compiler.cppstd=20
