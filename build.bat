6aa407c5b231e6ce398ecdc63ee3a236fe342f6e
NingLeiXueR <348634371@qq.com>
2026-03-09 16:04:58 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 conan profile detect --force
-#根据路径修改编译器版本 compiler.cppstd=20
-#-c "tools.files.download:verify=False" 设置不进行ssl验证(确保网络环境安全)
-#conan install . --output-folder=build/Release --build=missing -s build_type=Release -c "tools.files.download:verify=False" -s compiler.cppstd=20
-#conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20
-#conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20 -o abseil/*:shared=False -o protobuf/*:shared=False
+REM 根据路径修改编译器版本 compiler.cppstd=20
+REM -c "tools.files.download:verify=False" 设置不进行ssl验证(确保网络环境安全)
+REM conan install . --output-folder=build/Release --build=missing -s build_type=Release -c "tools.files.download:verify=False" -s compiler.cppstd=20
+REM conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20
+REM conan install . --output-folder=build/Debug --build=missing -s build_type=Debug -c "tools.files.download:verify=False" -s compiler.cppstd=20 -o abseil/*:shared=False -o protobuf/*:shared=False
 
 
 conan install . -s build_type=Release -s compiler.cppstd=20
