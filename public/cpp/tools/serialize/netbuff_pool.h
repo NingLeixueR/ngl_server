140e0301ae727af8b24fe5f5da380929daaaaaab
NingLeiXueR <348634371@qq.com>
2026-03-12 14:34:50 +0800

    Update netbuff_pool.h

--- public/cpp/tools/serialize/netbuff_pool.h
+++ public/cpp/tools/serialize/netbuff_pool.h
@@ -15,11 +15,15 @@
 
 #include "tools/threadtools.h"
 #include "tools/time_wheel.h"
-#include "tools/log/nlog.h"
 
 #include <algorithm>
 #include <array>
+#include <cstdint>
+#include <cstring>
+#include <format>
+#include <iostream>
 #include <list>
+#include <new>
 
 #define ISUSENETPOOL	(true)
 
@@ -28,6 +32,12 @@ namespace ngl
 	template <int TINITBYTES, int TCOUNT>
 	class netbuff
 	{
+		struct block_head
+		{
+			uint32_t m_magic = 0;
+			int32_t m_index = -1;
+		};
+
 		std::array<std::list<char*>, TCOUNT> m_pool;
 		std::array<int32_t, TCOUNT>			 m_bytes;
 		const std::array<int32_t, TCOUNT>	 m_counts;
@@ -35,19 +45,40 @@ namespace ngl
 
 		enum
 		{
-			enum_int32char = sizeof(int32_t) + sizeof(char),
+			enum_block_magic = 0x4E474C42,
+			enum_head_bytes = sizeof(block_head),
 		};
+
+		static void write_head(char* abuff, int32_t aindex)
+		{
+			block_head lhead
+			{
+				.m_magic = enum_block_magic,
+				.m_index = aindex,
+			};
+			memcpy(abuff, &lhead, sizeof(lhead));
+		}
+
+		static bool read_head(const char* abuff, block_head& ahead)
+		{
+			if (abuff == nullptr)
+			{
+				return false;
+			}
+			memcpy(&ahead, abuff, sizeof(ahead));
+			return ahead.m_magic == enum_block_magic;
+		}
 
 		char* nmalloc(int32_t aindex, int32_t abytes = 0)
 		{
 			int32_t lbytes = 0;
 			if (aindex >= TCOUNT || aindex < 0)
 			{
-				lbytes = abytes + enum_int32char;
+				lbytes = abytes + enum_head_bytes;
 			}
 			else
 			{
-				lbytes = m_bytes[aindex] + enum_int32char;
+				lbytes = m_bytes[aindex] + enum_head_bytes;
 			}
 			if (lbytes <= 0)
 			{
@@ -59,9 +90,8 @@ namespace ngl
 			{
 				return nullptr;
 			}
-			lpbuff[0] = 'k';
-			*(int32_t*)(&lpbuff[1]) = aindex;
-			return &lpbuff[enum_int32char];
+			write_head(lpbuff, aindex);
+			return &lpbuff[enum_head_bytes];
 		}
 
 		void nfree(char* abuff, bool abool = true)
@@ -70,13 +100,14 @@ namespace ngl
 			{
 				return;
 			}
-			char* lbuff = abuff - enum_int32char;
-			if (*lbuff != 'k')
+			char* lbuff = abuff - enum_head_bytes;
+			block_head lhead;
+			if (!read_head(lbuff, lhead))
 			{
-				log_error()->print("netbuff<{},{}>::nfree != k", TINITBYTES, TCOUNT);
+				std::cout << std::format("netbuff<{},{}>::nfree invalid head", TINITBYTES, TCOUNT) << std::endl;
 				return;
 			}
-			int lindex = *(int*)(abuff - sizeof(int));
+			const int32_t lindex = lhead.m_index;
 			if (lindex >= 0 && lindex < TCOUNT)
 			{
 				if (abool)
@@ -98,6 +129,7 @@ namespace ngl
 		netbuff(const std::array<int32_t, TCOUNT>& acounts):
 			m_counts(acounts)
 		{
+			static_assert(TCOUNT > 0, "netbuff requires at least one bucket");
 			m_bytes[0] = TINITBYTES;
 			for (int32_t i = 0; i < TCOUNT; ++i)
 			{
@@ -108,7 +140,10 @@ namespace ngl
 				for (int j = 0; j < m_counts[i]; ++j)
 				{
 					char* lbuff = nmalloc(i);
-					m_pool[i].push_back(lbuff);
+					if (lbuff != nullptr)
+					{
+						m_pool[i].push_back(lbuff);
+					}
 				}
 			}
 
@@ -127,6 +162,10 @@ namespace ngl
 
 		char* malloc_private(int32_t abytes)
 		{
+			if (abytes <= 0)
+			{
+				return nullptr;
+			}
 			auto itor = std::lower_bound(m_bytes.begin(), m_bytes.end(), abytes);
 			if (itor != m_bytes.end())
 			{
