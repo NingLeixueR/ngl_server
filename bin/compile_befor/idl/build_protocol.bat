57c20ae9af47129436d3d81ac483d704e8b4a186
NingLeiXueR <348634371@qq.com>
2026-03-26 16:37:37 +0800

    Revert "优化：路径调整"
    This reverts commit 0324bd486e1d14022947c7a326b614ba19d2d397.

--- bin/compile_befor/idl/build_protocol.bat
+++ bin/compile_befor/idl/build_protocol.bat
@@ -1,27 +1,5 @@
-@echo off
 REM File overview: Automates the build protocol workflow on Windows.
 
-setlocal
-
-set "lbat=%~dp0"
-for %%I in ("%lbat%..\..\..") do set "lroot=%%~fI"
-set "lbin=%lroot%\bin\Debug"
-set "lauto=%lroot%\public\cpp\actor\generated\auto"
-
-if not exist "%lbin%\IDL.exe" (
-	echo Missing tool: "%lbin%\IDL.exe"
-	exit /b 1
-)
-
-if not exist "%lauto%" mkdir "%lauto%"
-
-pushd "%lbin%" || exit /b 1
-IDL.exe protocol || (
-	popd
-	exit /b 1
-)
-copy /y "nprotocol_auto.h" "%lauto%\" >nul || (
-	popd
-	exit /b 1
-)
-popd
+cd ../../Debug/
+IDL.exe protocol
+xcopy nprotocol_auto.h ..\..\public\cpp\actor\generated\auto\ /s/y
--- bin/compile_befor/proto/build_protocol.bat
+++ bin/compile_befor/proto/build_protocol.bat
@@ -1,74 +1,23 @@
-@echo off
 REM File overview: Automates the build protocol workflow on Windows.
 
-setlocal
+mkdir pro
+protoc.exe --version
+protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro net.proto
+protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro db.proto
+protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro example.proto
 
-set "lbat=%~dp0"
-for %%I in ("%lbat%..\..\..") do set "lroot=%%~fI"
-set "lbin=%lroot%\bin\Debug"
-set "lpro=%lbat%pro"
-set "lprotoc=%lbat%protoc.exe"
-set "lpb=%lroot%\public\cpp\actor\generated\pb"
-set "lauto=%lroot%\public\cpp\actor\generated\auto"
+protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro net.proto
+protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro db.proto
+protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro example.proto
 
-if not exist "%lprotoc%" (
-	echo Missing tool: "%lprotoc%"
-	exit /b 1
-)
+xcopy pro\*.cc ..\..\..\public\cpp\actor\generated\pb\ /s/y
+xcopy pro\*.h ..\..\..\public\cpp\actor\generated\pb\ /s/y
+#xcopy pro\*.cs ..\..\..\public\csharp\csharp\csharp\protobuf /s/y
 
-if not exist "%lbin%\makeproto.exe" (
-	echo Missing tool: "%lbin%\makeproto.exe"
-	exit /b 1
-)
-
-if not exist "%lpro%" mkdir "%lpro%"
-if not exist "%lpb%" mkdir "%lpb%"
-if not exist "%lauto%" mkdir "%lauto%"
-
-pushd "%lbat%" || exit /b 1
-"%lprotoc%" --version || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --cpp_out="%lpro%" net.proto || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --cpp_out="%lpro%" db.proto || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --cpp_out="%lpro%" example.proto || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --csharp_out="%lpro%" net.proto || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --csharp_out="%lpro%" db.proto || (
-	popd
-	exit /b 1
-)
-"%lprotoc%" --experimental_allow_proto3_optional --csharp_out="%lpro%" example.proto || (
-	popd
-	exit /b 1
-)
-popd
-
-if exist "%lpro%\*.cc" xcopy "%lpro%\*.cc" "%lpb%\" /i /y >nul || exit /b 1
-if exist "%lpro%\*.h" xcopy "%lpro%\*.h" "%lpb%\" /i /y >nul || exit /b 1
-REM xcopy "%lpro%\*.cs" "%lroot%\public\csharp\csharp\csharp\protobuf\" /i /y
-
-pushd "%lbin%" || exit /b 1
-makeproto.exe "%lbat%" || (
-	popd
-	exit /b 1
-)
-for %%I in (pb_protocol.cpp pbname_match.cpp nscript_pbdb.h nscript_pbexample.h nscript_pbnet.h) do (
-	copy /y "%%I" "%lauto%\" >nul || (
-		popd
-		exit /b 1
-	)
-)
-popd
+cd ../../Debug/
+makeproto.exe ../compile_befor/proto
+xcopy pb_protocol.cpp ..\..\public\cpp\actor\generated\auto\ /s/y
+xcopy pbname_match.cpp ..\..\public\cpp\actor\generated\auto\ /s/y
+xcopy nscript_pbdb.h ..\..\public\cpp\actor\generated\auto\ /s/y
+xcopy nscript_pbexample.h ..\..\public\cpp\actor\generated\auto\ /s/y
+xcopy nscript_pbnet.h ..\..\public\cpp\actor\generated\auto\ /s/y
