2c8801afa262683fc31b2d69ef0cdad00c32cb27
n <348634371@qq.com>
2026-04-06 21:40:48 +0800

    Update start_client.bat

--- bin/configure/windows/start_client.bat
+++ bin/configure/windows/start_client.bat
@@ -3,5 +3,6 @@ REM File overview: Automates the start client workflow on Windows.
 set /P rebot_value1=区服id
 set /P rebot_value2=实例id
 set /P rebot_value3=请输入玩家id
-start node_robot robot %rebot_value1% %rebot_value2% libo %rebot_value3% %rebot_value3%
+set /P rebot_value4=协议(ws,wss,tcp)
+start node_robot robot %rebot_value1% %rebot_value2% libo %rebot_value3% %rebot_value3% %rebot_value4%
 #robot 1 1 libo 7777 7777
