af1c044f90e7a39a852407f7a4b21b33bb14deed
NingLeiXueR <3366077+NingLeixueR@users.noreply.github.com>
2026-04-21 11:51:37 +0800

    Update close_actor_html.php

--- admin/sys/close_actor_html.php
+++ admin/sys/close_actor_html.php
@@ -1,6 +1,6 @@
 <?php require_once dirname(__FILE__) . '/../auth.php'; check_action(1501); ?>
 <html>
-	<h2>关闭guid</h2>
+	<h2>关闭actor</h2>
 	<form action="./close_actor.php" method="post" accept-charset="UTF-8">
 		<?php require_once "../serverls.php";?>
 		actor_name:<input name="actor_name" type="text"/><br/>
