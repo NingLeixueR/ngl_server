0e433982ed45ed2bc64bb48ed9f892d20d5b3234
NingLeiXueR <3366077+NingLeixueR@users.noreply.github.com>
2026-04-24 09:49:35 +0800

    Update nbt.h

--- public/cpp/tools/ai/nbt.h
+++ public/cpp/tools/ai/nbt.h
@@ -1,14 +1,19 @@
+/*
+* Copyright (c) [2020-2025] NingLeixueR
+*
+* Project name: ngl_server
+* Project URL: https://github.com/NingLeixueR/ngl_server
+*
+* This file is part of the ngl_server project and is distributed under the MIT License.
+* You may use, modify, and distribute this project under the license, including commercial use,
+* but you must retain the original copyright and license notice.
+*
+* For license details, see the LICENSE file in the project root:
+* https://github.com/NingLeixueR/ngl_server/blob/main/LICENSE
+*/
 #pragma once
 
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4702)
-#endif
 #include <behaviortree_cpp/bt_factory.h>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-
 #include <unordered_map>
 #include <functional>
 #include <memory>
